e234b85f94ec448dfb105f25e5500aa5fb59fa95
muqsitamir <islammuqsit7@gmail.com>
2023-01-21 02:49:53 +0500

    Changed database in settings/prod.py

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -26,7 +26,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['172.105.61.72', 'api.tpilums.org.pk']
 
-ALLOWED_HOSTS += ['127.0.0.1', '192.168.0.12']
+ALLOWED_HOSTS += ['127.0.0.1', '192.168.0.12', "139.162.11.234"]
 # Application definition
 
 DEFAULT_APPS = [
