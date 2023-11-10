48c8ce2ef9d1f558eec20779f0bed12799a7e339
muqsitamir <islammuqsit7@gmail.com>
2023-11-10 22:15:20 +0500

    check disk space copied from cam trap repo

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -215,3 +215,12 @@ WEBPACK_LOADER = {
 }
 
 MODEL_SERVICE_URL = 'http://203.135.63.37:5000'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'lumscameratrap@gmail.com'
+EMAIL_HOST_PASSWORD = 'xzwr fguu icqv jxlv'
+DEFAULT_FROM_EMAIL = "lumscameratrap@gmail.com"
