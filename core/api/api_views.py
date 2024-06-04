c76acc34711393dab1fdb3cbbf928288ead8def6
zainulhassan540 <zainulhassan540@gmail.com>
2024-06-05 04:12:09 +0500

    api function added for eventCount table

--- core/api/api_views.py
+++ core/api/api_views.py
@@ -382,8 +382,8 @@ class EventCountViewSet(viewsets.ModelViewSet):
     serializer_class = EventCountSerializer
 
     @action(detail=False, methods=['get'])
-    def get_queryset(self, request):
-        camera_id = request.query_params.get('camera_id')
+    def get_queryset(self):
+        camera_id = self.request.query_params.get('camera_id')
         if camera_id:
             event_count = EventCount.objects.filter(camera_id=camera_id).first()
             if event_count:
