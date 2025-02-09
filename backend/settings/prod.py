f7e48612d257ceef8dc6727f7416a19f938dadf4
muqsitamir <islammuqsit7@gmail.com>
2025-02-09 21:38:44 +0500

    Stopping the load on AI api

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -228,7 +228,7 @@ WEBPACK_LOADER = {
     }
 }
 
-MODEL_SERVICE_URL = 'http://203.135.63.37:5000'
+MODEL_SERVICE_URL = 'http://203.135.63.37:50001'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
