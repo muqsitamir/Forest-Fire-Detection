832694018acc37f2ed71d68070005b29a850c788
muqsitamir <islammuqsit7@gmail.com>
2025-03-05 14:50:41 +0500

    testing reordering url custom path in accounts/urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     # path('', include('django.contrib.auth.urls')),
     # path('signup/', views.signup, name='signup'),
+    path('api/', include(('accounts.api.urls', 'accounts'), namespace='accounts-api')),
     path('api/', include('djoser.urls'), name='auth_api'),
     path('api/', include('djoser.urls.authtoken')),
-    path('api/', include(('accounts.api.urls', 'accounts'), namespace='accounts-api')),
 ]
